1f3056bcaf42627030c9ae654be0f783048d1634
Tyson Thomas <tysonthomas.90@gmail.com>
2016-08-04 00:48:55 +0530

    Added the application db query

--- server/routes/top-active-users.js
+++ server/routes/top-active-users.js
@@ -2,15 +2,50 @@
 
 var express = require('express');
 var router = express.Router();
+var Q = require('q');
 
-var pgp = require('pg-promise')({});
+var options = {
+    promiseLib: Q
+};
+
+var pgp = require('pg-promise')(options);
 const conString = 'postgres://postgres@localhost/testdb1';
 var db = pgp(conString);
 
 /* GET top active users listing. */
 router.get('/', function(req, res, next) {
     let pageNumber = parseInt(req.query.page);
-    res.json({message: 'done'});
+    let requests = [];
+    var outputData;
+
+    //execute the query
+    db.any('SELECT users.id, users.created_at, users.name, ctab.count FROM users INNER JOIN (SELECT user_id, COUNT(user_id) AS "count" from applications WHERE created_at > \'20160730\' GROUP BY user_id) AS ctab ON users.id=ctab.user_id ORDER BY users.id LIMIT 5')
+    .then(function (users) {
+      users.forEach(function(user) {
+        requests.push(db.any('SELECT app.user_id, listings.name from applications AS app INNER JOIN listings ON app.listing_id=listings.id WHERE app.user_id=$1::int AND app.created_at > \'20160730\' ORDER BY app.created_at DESC LIMIT 3;', [user.id]));
+      });
+
+      Q.all(requests)
+      .then(function(listings) {
+        listings.forEach(function(listingArray) {
+          var newListings = listingArray.map(function (listings) {
+            return listings.name;
+          });
+
+          users.forEach(function(user) {
+            if(user.id === listingArray[0].user_id){
+              user.listings = newListings;
+            }
+          });
+        });
+
+        res.json(users);
+      });
+    })
+    .catch(function (err) {
+      return next({message: err.message});
+    });
+
 });
 
 module.exports = router;
