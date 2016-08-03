0aa304fba0f6d5d4bdff9fce87978419a89e6e11
Tyson Thomas <tysonthomas.90@gmail.com>
2016-08-04 02:58:03 +0530

    Added comments  and minor updates

--- server/routes/top-active-users.js
+++ server/routes/top-active-users.js
@@ -4,33 +4,47 @@ var express = require('express');
 var router = express.Router();
 var Q = require('q');
 
-var options = {
+const options = {
     promiseLib: Q
 };
 
 var pgp = require('pg-promise')(options);
-const conString = 'postgres://postgres@localhost/testdb1';
+const conString = process.env.DB_URL || 'postgres://brvmmhgs6x:puzlacdvls@assignment.codsssqklool.eu-central-1.rds.amazonaws.com:5432/brvmmhgs6x_db';
 var db = pgp(conString);
 
 /* GET top active users listing. */
 router.get('/', function(req, res, next) {
+    //send error message if its not a number
+    if(isNaN(req.query.page)){
+      return res.status(400).send({
+        message: 'page not a Number'
+      });
+    }
+
     let pageNumber = parseInt(req.query.page);
     let requests = [];
 
-    //execute the query
+    //execute the query to fetch the user details and count
     db.any('SELECT users.id, users.created_at, users.name, ctab.count FROM users INNER JOIN (SELECT user_id, COUNT(user_id) AS "count" from applications WHERE created_at > (CURRENT_DATE - INTERVAL \'7 day\') GROUP BY user_id) AS ctab ON users.id=ctab.user_id ORDER BY users.id LIMIT 5')
     .then(function (users) {
       users.forEach(function(user) {
+        //execute the query to fetch the user 3 last listings data
         requests.push(db.any('SELECT app.user_id, listings.name from applications AS app INNER JOIN listings ON app.listing_id=listings.id WHERE app.user_id=$1::int AND app.created_at > (CURRENT_DATE - INTERVAL \'7 day\') ORDER BY app.created_at DESC LIMIT 3;', [user.id]));
       });
 
+      //wait for all the query requests to complete
       Q.all(requests)
       .then(function(listings) {
+
+        //iterate over all the reuests
         listings.forEach(function(listingArray) {
-          var newListings = listingArray.map(function (listings) {
+
+          //ceate a new array of listings for this user
+          let newListings = listingArray.map(function (listings) {
             return listings.name;
           });
 
+          //iterate over the list of users to find a match
           users.forEach(function(user) {
             if(user.id === listingArray[0].user_id){
               user.listings = newListings;
@@ -38,6 +52,7 @@ router.get('/', function(req, res, next) {
           });
         });
 
+        //map the output data as the spec document
         let outputUserData = users.map(function (user) {
           return {
             id: user.id,
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,11 +4,18 @@ var express = require('express');
 var router = express.Router();
 
 var pgp = require('pg-promise')({});
-const conString = 'postgres://postgres@localhost/testdb1';
+const conString = process.env.DB_URL || 'postgres://brvmmhgs6x:puzlacdvls@assignment.codsssqklool.eu-central-1.rds.amazonaws.com:5432/brvmmhgs6x_db';
 var db = pgp(conString);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+    //send error message if its not a number
+    if(isNaN(req.query.id)){
+      return res.status(400).send({
+        message: 'id not a Number'
+      });
+    }
+
     let userId = parseInt(req.query.id);
 
     let user;
@@ -16,71 +23,83 @@ router.get('/', function(req, res, next) {
     let createdListings = [];
     let applications = [];
 
-    //execute the query
+    //execute the query to fetch the user profile
     db.oneOrNone('SELECT id, name, created_at FROM users WHERE id=$1::int', [userId])
     .then(function (resultUser) {
+      //send 'not found' message if user profile not found
       if(!resultUser){
-        return res.json({message: 'No user found'});
+        return res.status(404).send({
+          message: 'user not found'
+        });
       }
 
       user = resultUser;
-    })
-    .then(function() {
-      return db.any('SELECT com.id, com.created_at, com.name, teams.contact_user from companies AS com INNER JOIN teams ON com.id=teams.company_id WHERE user_id=$1::int', [userId]);
-    })
-    .then(function (resultCompanies) {
-      resultCompanies.forEach(function(row) {
-        companies.push({
-          id: row.id,
-          createdAt: row.created_at,
-          name: row.name,
-          isContact: row.contact_user
-        });
-      });
-    })
-    .then(function() {
-      return db.any('SELECT id, created_at, name, description FROM listings WHERE created_by=$1::int', [userId]);
-    })
-    .then(function (resultListings) {
-      resultListings.forEach(function(row) {
-        createdListings.push({
-          id: row.id,
-          createdAt: row.created_at,
-          name: row.name,
-          description: row.description
-        });
-      });
-    })
-    .then(function() {
-      return db.any('SELECT app.id, app.created_at, app.cover_letter, list.id AS listings_id, list.name AS listings_name, list.description AS listings_description from applications AS app INNER JOIN listings AS list ON app.listing_id=list.id WHERE user_id=$1::int', [userId]);
-    })
-    .then(function (resultApplications) {
-      resultApplications.forEach(function(row) {
-        applications.push({
-          id: row.id,
-          createdAt: row.created_at,
-          listing: {
-            id: row.listings_id,
-            name: row.listings_name,
-            description: row.listings_description
-          },
-          cover_letter: row.cover_letter
-        });
-      });
-    })
-    .then(function () {
-      var resultOutput = {
-        id: user.id,
-        name: user.name,
-        createdAt: user.created_at,
-        companies : companies,
-        createdListings : createdListings,
-        applications: applications
-      };
 
-      res.json(resultOutput);
+      //execute the query to fetch the companies data
+      db.any('SELECT com.id, com.created_at, com.name, teams.contact_user from companies AS com INNER JOIN teams ON com.id=teams.company_id WHERE user_id=$1::int', [userId])
+      .then(function (resultCompanies) {
+        resultCompanies.forEach(function(row) {
+          companies.push({
+            id: row.id,
+            createdAt: row.created_at,
+            name: row.name,
+            isContact: row.contact_user
+          });
+        });
+      })
+      .then(function() {
+        //execute the query to fetch the listings data
+        return db.any('SELECT id, created_at, name, description FROM listings WHERE created_by=$1::int', [userId]);
+      })
+      .then(function (resultListings) {
+        resultListings.forEach(function(row) {
+          createdListings.push({
+            id: row.id,
+            createdAt: row.created_at,
+            name: row.name,
+            description: row.description
+          });
+        });
+      })
+      .then(function() {
+        //execute the query to fetch the applications data
+        return db.any('SELECT app.id, app.created_at, app.cover_letter, list.id AS listings_id, list.name AS listings_name, list.description AS listings_description from applications AS app INNER JOIN listings AS list ON app.listing_id=list.id WHERE user_id=$1::int', [userId]);
+      })
+      .then(function (resultApplications) {
+        resultApplications.forEach(function(row) {
+          applications.push({
+            id: row.id,
+            createdAt: row.created_at,
+            listing: {
+              id: row.listings_id,
+              name: row.listings_name,
+              description: row.listings_description
+            },
+            cover_letter: row.cover_letter
+          });
+        });
+      })
+      .then(function () {
+        //map the data to spec format
+        var resultOutput = {
+          id: user.id,
+          name: user.name,
+          createdAt: user.created_at,
+          companies : companies,
+          createdListings : createdListings,
+          applications: applications
+        };
+
+        //send the data to client
+        res.json(resultOutput);
+      })
+      .catch(function (err) {
+        //send server error message
+        return next({message: err.message});
+      });
     })
     .catch(function (err) {
+      //send server error message
       return next({message: err.message});
     });
 });
